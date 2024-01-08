3cc499fc91bb638176fa5813a7fbcf50335b3764
DenysTkachov <D.Tkachev@ukr.net>
2024-01-08 18:25:02 +0100

    4

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,56 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person entered the house.");
+    } else {
+      console.log("Door is closed. Person cannot enter.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open");
+    } else {
+      console.log("Wrong house. Door closed");
+    }
+  }
+}
+
+
+
+
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
